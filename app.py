67db8ea0dbcd167275cb14f565beac857111259e
Curt Micol <asenchi@asenchi.com>
2011-11-28 23:06:41 -0500

    Yay it works -- sort of.

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import os
 import yaml
-from flask import Flask
+from flask import Flask, render_template
 
 SONGS = "./songs"
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-def open_song(s):
+def open_file(s):
     return yaml.load(file(s, 'r').read())
 
-def struct(d):
+def build_song(d):
     if d["structure"]:
         d["final"] = []
         for s in d["structure"]:
@@ -18,45 +18,49 @@ def struct(d):
     d["final"] = "\n".join(d["final"])
     return d
 
-def get_songs(d):
-    for song in os.listdir(d):
-        path = os.path.join(d, song)
+def get_files(directory):
+    for filename in os.listdir(directory):
+        path = os.path.join(directory, filename)
         if os.path.isfile(path):
-            yield (song, path)
+            yield (filename, path)
 
-def cleanse_file(f):
+def pretty_filename(f):
     return f.replace("_", " ").title().split('.')[0]
 
 def toc(songs):
     sl = []
-    sl.append("<pre>\n")
     for song in songs:
-        sl.append("* %s" % cleanse_file(song[0]))
-    sl.append("</pre>\n")
-    return "\n".join(sl)
+        filename = song[0].replace("_", "-").split('.')[0]
+        sl.append((filename, pretty_filename(song[0])))
+    return list(enumerate(sl, start=1))
 
 @app.route("/songs/")
 def songs():
     songlist = []
-    for song in get_songs(app.config["SONGS"]):
+    for song in get_files(app.config["SONGS"]):
         songlist.append(song)
     songlist.sort()
     songs = toc(songlist)
-    return songs
+    return render_template(
+               "toc.html",
+               songs=songs,
+               title="Table of Contents"
+           )
 
 @app.route("/song/<title>")
 def song(title):
-    render = []
+    # We use '_' for filenames, match this with what we find.
     cleanse = title.replace("-", "_")
-    songlist = get_songs(app.config["SONGS"])
+    songlist = get_files(app.config["SONGS"])
     for song, path in songlist:
         if song.split('.')[0] == cleanse:
-            opened = open_song(path)
-            compiled = struct(opened)
-            render.append("<pre>\n")
-            render.append(compiled["final"])
-            render.append("</pre>\n")
-    return "\n".join(render)
+            opened = open_file(path)
+            compiled = build_song(opened)
+    return render_template(
+               "song.html",
+               song=compiled["final"],
+               title=compiled["title"]
+           )
 
 @app.route("/help")
 def help():
