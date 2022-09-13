9ed2dc6a55c00ee13103dfc76bff037a88b37101
Curt Micol <asenchi@asenchi.com>
2022-09-12 20:16:34 -0400

    Run our "freeze", which turns all pages into static html
    This will change our server from a dynamic to static site. This is required
    because we are moving off of Heroku and the site is too complex. Moving to
    static files makes it easy to serve from anywhere.
    
    I need to add workflows for generating the pages each time we make changes, but
    for now this is the initial snapshot.

---
+++ build/static/slides.js
@@ -0,0 +1,55 @@
+window.onload = function() {
+	var slides = document.getElementById('slides');
+	var progress = document.getElementById('progress');
+	if(!slides) return;  // Not showing a song, nothing to do
+
+	// Put together a list of slides
+	var anchors = slides.getElementsByTagName('a'), slides = [];
+	for(var i=0; i < anchors.length; i++) {
+		var index = anchors[i].name;
+		var slide = document.getElementById('slide' + index);
+		slides.push(slide);
+	}
+
+	// Change slides as necessary
+	var current = 0;
+	function activate(index) {
+		if(index < 0 || index >= slides.length) return;  // Out of bounds
+		slides[current].style.display = 'none';
+		slides[index].style.display = '';
+		current = index;
+		progress.innerHTML = (current + 1) + '/' + slides.length;
+	}
+
+	// Respond to keyboard events
+	document.onkeydown = function(event) {
+		if(event.altKey || event.shiftKey) return;
+		switch(event.keyCode) {
+			case 13:  // Enter
+			case 32:  // Space
+			case 34:  // Page Down
+			case 39:  // Right
+			case 40:  // Down
+				activate(current + 1);
+				event.preventDefault();
+				return false;
+			case 8:   // Backspace
+			case 33:  // Page Up
+			case 37:  // Left
+			case 38:  // Up
+				activate(current - 1);
+				event.preventDefault();
+				return false;
+			case 66:  // B
+				var hidden = slides[current].style.display == 'none';
+				slides[current].style.display = hidden ? '' : 'none';
+				event.preventDefault();
+				return false;
+			case 84:  // T
+				window.location.href = '/songs/';
+				event.preventDefault();
+				return false;
+                        case 86:  // V
+		}
+	}
+}
